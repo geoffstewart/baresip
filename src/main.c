cbeadeacab4ba2903d1770a7c632ec6dbe59cc33
Geoff Stewart <gstewart@martellotech.com>
2017-11-13 14:54:47 -0500

    allow specify the network interface on the command line

--- src/main.c
+++ src/main.c
@@ -59,6 +59,7 @@ static void usage(void)
 			 "\t-p <path>        Audio files\n"
 			 "\t-h -?            Help\n"
 			 "\t-t               Test and exit\n"
+			 "\t-n <net_if>      Specify network interface\n"
 			 "\t-u <parameters>  Extra UA parameters\n"
 			 "\t-v               Verbose debug\n"
 			 );
@@ -69,6 +70,7 @@ int main(int argc, char *argv[])
 {
 	bool prefer_ipv6 = false, run_daemon = false, test = false;
 	const char *ua_eprm = NULL;
+	const char *net_interface = NULL;
 	const char *execmdv[16];
 	const char *audio_path = NULL;
 	const char *modv[16];
@@ -90,7 +92,7 @@ int main(int argc, char *argv[])
 
 #ifdef HAVE_GETOPT
 	for (;;) {
-		const int c = getopt(argc, argv, "6de:f:p:hu:vtm:");
+		const int c = getopt(argc, argv, "6de:f:p:hun:vtm:");
 		if (0 > c)
 			break;
 
@@ -135,6 +137,10 @@ int main(int argc, char *argv[])
 			modv[modc++] = optarg;
 			break;
 
+		case 'n':
+			net_interface = optarg;
+			break;
+
 		case 'p':
 			audio_path = optarg;
 			break;
@@ -170,6 +176,10 @@ int main(int argc, char *argv[])
 	 * Initialise the top-level baresip struct, must be
 	 * done AFTER configuration is complete.
 	 */
+	struct config *theconf = conf_config();
+	if (net_interface) {
+		str_ncpy(theconf->net.ifname, net_interface, sizeof(theconf->net.ifname));
+	}
 	err = baresip_init(conf_config(), prefer_ipv6);
 	if (err) {
 		warning("main: baresip init failed (%m)\n", err);
